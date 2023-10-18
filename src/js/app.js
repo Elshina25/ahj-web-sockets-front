e262b6a9602f8d3d542d1ce22138c92b37c70252
Elshina25 <102738101+Elshina25@users.noreply.github.com>
2023-10-19 02:01:23 +0300

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,6 @@ import Chat from "./Chat/Chat";
 
 const container = document.querySelector(".container");
 //const url = "ws://localhost:7070/ws";
-const url = "ws:https://elshina25.github.io/ahj-web-sockets-front:7070/ws";
+const url = "wss://ws-backend-781j.onrender.com/ws";
 const chat = new Chat(url, container);
 chat.createPopup();
